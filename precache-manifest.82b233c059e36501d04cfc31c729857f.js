168fe7dde059a18e09aeaff6665b25de1346fe51
Kandiak Oleg <kandio1604@gmail.com>
2021-07-29 15:39:35 +0300

    Updates

---
+++ precache-manifest.82b233c059e36501d04cfc31c729857f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9c2be570bf8ebb011c67136aaf263c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c9f308592b99278309f",
+    "url": "./static/css/main.95b32d11.chunk.css"
+  },
+  {
+    "revision": "3c5c45168a7197cfd152",
+    "url": "./static/js/2.8a4bc3be.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.8a4bc3be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9f308592b99278309f",
+    "url": "./static/js/main.c4a3ab8b.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
